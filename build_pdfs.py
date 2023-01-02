24e4cb08090eb90de1b9a24a91055a86f3d2e822
Ash <maybork@pm.me>
2023-01-02 14:50:13 +0100

    parameterize wkhtmltopdf options

--- build_pdfs.py
+++ build_pdfs.py
@@ -6,11 +6,11 @@ EN_DIST = "http://localhost:3000/astro_resume/en"
 EN_TARGET = "./public/en.pdf"
 PL_DIST = "http://localhost:3000/astro_resume/pl"
 PL_TARGET = "./public/pl.pdf"
-
+OPTS = "--print-media-type --no-outline"
 dev = subprocess.Popen("npm run astro dev", shell=True, stdout=None)
 time.sleep(5)
 
-if subprocess.Popen(f"wkhtmltopdf --print-media-type {EN_DIST} {EN_TARGET} ", shell=True, stdout=None).wait() or subprocess.Popen(f"wkhtmltopdf --print-media-type {PL_DIST} {PL_TARGET}", shell=True, stdout=None).wait():
+if subprocess.Popen(f"wkhtmltopdf {OPTS} {EN_DIST} {EN_TARGET} ", shell=True, stdout=None).wait() or subprocess.Popen(f"wkhtmltopdf  {OPTS} {PL_DIST} {PL_TARGET}", shell=True, stdout=None).wait():
     print("PDF build failed")
     raise SystemExit(1)
 print("PDF build successful!")
