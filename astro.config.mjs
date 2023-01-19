0e20f388775ac28ae74f4bbe1b9c75d3bcb64254
Ash <82605611+ashbork@users.noreply.github.com>
2023-01-19 10:57:00 +0100

    Update astro.config.mjs
    Match new GitHub username

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: "https://maybork.github.io",
+    site: "https://ashbork.github.io",
     base: "astro_resume"
 });
